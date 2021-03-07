93cbe33b52df70861101e8280cd06f8e739b65ce
tahmed4 <55896104+tahmed4@users.noreply.github.com>
2021-03-07 12:52:47 +0000

    update docs to reflect new error throwing

--- ubfToTwilio.js
+++ ubfToTwilio.js
@@ -19,7 +19,9 @@ class Api {
    * @param {string} twilioAccountSid - the Twilio Account ID.
    * @param {string} twilioAuthToken - the Twilio Auth Token.
    * 
-   * @returns {Object} - client object to do API calls. `null` on error case.
+   * @returns {Object} - client object to do API calls. `null` if login details were invalid.
+   * 
+   * @throws {TwilioRequestError} - If twilio request to login fails.
    */
   async tryConnectingToTwilio(twilioAccountSid, twilioAuthToken) {
       var client = await connect.twilioConnect(twilioAccountSid, twilioAuthToken)
@@ -29,11 +31,13 @@ class Api {
   /**
    * Returns all phone numbers under this account's control
    * including numbers that are currently in use by bots
-   * or available. 
+   * or available.
    * 
    * @param {Object} client - Twilio API client object.
    * 
-   * @returns {string[]} - all phonenumbers on twilio Account. `null` on error case.
+   * @returns {string[]} - all phonenumbers on twilio Account.
+   * 
+   * @throws {TwilioRequestError} - If retrieving account phone numbers fail.
    */
   async getSMSAccountPhoneNumbers(client) {
     return (await phonenumbers.getSMSAccountPhoneNumbers(client))
@@ -48,7 +52,10 @@ class Api {
    * 
    * @param {Object} client - Twilio API client object.
    * 
-   * @returns {Object[]} - All fully initialised, except for diagram, live bots. `null` on error case.
+   * @returns {Object[]} - All fully initialised, except for diagram, live bots. 
+   * 
+   * @throws {TwilioRequestError} - If Twilio request to get numbers or bot data fails.
+   * @throws {Error} - If numbers or data retrieved are empty.
    */
   async getDeployedBots(client) {
       return(await deployed.getDeployedBots(client))
@@ -64,7 +71,9 @@ class Api {
    * @param {Object} client -  Twilio API client object.
    * @param {Object} bot - Defined bot that is about to be uploaded.
    * 
-   * @returns {string} - Twilio assistant unique id. `null` on error case.
+   * @returns {string} - Twilio assistant unique id. 
+   * 
+   * @throws {ValidationError} - If bot definition is missing any data.
    */
   async uploadNewBot(client, bot) {
       if(bot == null){
@@ -81,7 +90,10 @@ class Api {
    * @param {Object} client - Twilio API client object.
    * @param {string} id - Twilio Assistant id of bot to be removed.
    * 
-   * @returns {boolean} - `true` if bot successfully removed, otherwise `false`
+   * @returns {boolean} - `true` if bot successfully removed.
+   * 
+   * @throws {ValidationError} - If bot trying to be removed doesn't exist.
+   * @throws {TwilioRequestError} - If requests to Twilio to remove bot fail.
    */
   async removeBot(client, id) {
       return await remove.removeBot(client, id)
@@ -98,7 +110,9 @@ class Api {
    * @param {string} id - id of assistant that is going to be associated with number.
    * @param {string} number - Desired phone number for assistant to be linked to. e.g ("+447123456789")
    * 
-   * @returns {boolean} `true` if number successfully changed, otherwise `false`
+   * @returns {boolean} - `true` if number successfully changed.
+   * 
+   * @throws {TwilioRequestError} - throws error if number didn't change successfully
    */
   async changeBotPhoneNumber(client, id, number) {
     return changeNumber.changeBotPhoneNumber(client, id, number)
@@ -113,7 +127,9 @@ class Api {
    * @param {string} id - id of assistant which you want to simulate.
    * @param {string} text - text message which you want to send to the bot.
    * 
-   * @returns {string[]} - text responses from simulated bot. `null` on error case.
+   * @returns {string[]} - text responses from simulated bot. 
+   * 
+   * @throws {TwilioRequestError} - If request to get simulated text fails.
    */
   async simulateBot(client, id, text){
     log.info("Simulating bot with text '" + text + "'")
@@ -128,7 +144,10 @@ class Api {
    *
    * @param {Object} client - Twilio API client object.
    * 
-   * @returns {Object[]} - list of unlinked bots. `null` on error case.
+   * @returns {Object[]} - list of unlinked bots.
+   * 
+   * @throws {TwilioRequestError} - If retrieving bot data fails.
+   * @throws {Error} - If data retrieved is empty.
    */
   async getUnlinkedBots(client){
     return (await unlinked.getUnlinkedBots(client))
