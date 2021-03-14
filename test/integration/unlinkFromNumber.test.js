c22c0b22b16ca862711fd601a39d74d6a17009a3
tahmed4 <55896104+tahmed4@users.noreply.github.com>
2021-03-14 16:27:21 +0000

    run test by differing input param

--- test/integration/unlinkFromNumber.test.js
+++ test/integration/unlinkFromNumber.test.js
@@ -2,52 +2,54 @@ const unlink = require("../../lib/unlinkFromNumber")
 const changeNumber = require("../../lib/changeBotPhoneNumber")
 const twilio = require("twilio")
 
+describe("Testing with real client", () => {
 
-test("Free deployed bot", async () => {
-    let client = twilio("AC16497ae92be880bf536ddf0d8ae92add", "9e9acd8216a57baac83559ee8df1ee00")
-    let resp = await unlink.unlinkFromPhoneNumber(client, "UAa556f1a138444cf5359020bba46f017e")
-    expect(resp).toBe(true)
-    await changeNumber.changeBotPhoneNumber(client, "UAa556f1a138444cf5359020bba46f017e", "+44778260206")
-    })
+    let client;
 
-test("Attempt to free undeployed assistant", async () => {
-    let client = twilio("AC16497ae92be880bf536ddf0d8ae92add", "9e9acd8216a57baac83559ee8df1ee00")
-    let resp = await unlink.unlinkFromPhoneNumber(client, "")
-    expect(resp).toBe(false)
-    })
+    beforeEach(() => {
+        client = twilio("AC16497ae92be880bf536ddf0d8ae92add", "9e9acd8216a57baac83559ee8df1ee00")
+      });
 
-test("Attempt to free null assistant", async () => {
-    let client = twilio("AC16497ae92be880bf536ddf0d8ae92add", "9e9acd8216a57baac83559ee8df1ee00")
-    let resp = await unlink.unlinkFromPhoneNumber(client, null)
-    expect(resp).toBe(false)
-    })
-
-test("Attempt to free undefined assistant", async () => {
-    let client = twilio("AC16497ae92be880bf536ddf0d8ae92add", "9e9acd8216a57baac83559ee8df1ee00")
-    let resp = await unlink.unlinkFromPhoneNumber(client, undefined)
-    expect(resp).toBe(false)
-    })
-
-
-test("Invalid Twilio Credentials", () => {
-    let client = twilio("ACInvalid", "Credentials")
-    unlink.unlinkFromPhoneNumber(client, "", "").catch(resp => {
-        expect(resp.name).toEqual("TwilioRequestError")
-    })
-})
-
-test("Null Client", () => {
-    let client = null
-    unlink.unlinkFromPhoneNumber(client, "").catch(resp => {
-        expect(resp.name).toEqual("TwilioRequestError")
-    })
-})
-
-test("Undefined Client", () => {
-    let client = undefined
-    unlink.unlinkFromPhoneNumber(client, "").catch(resp => {
-        expect(resp.name).toEqual("TwilioRequestError")
-    })
-})
+    test("Free deployed bot", async () => {
+        let resp = await unlink.unlinkFromPhoneNumber(client, "UAa556f1a138444cf5359020bba46f017e")
+        expect(resp).toBe(true)
+        await changeNumber.changeBotPhoneNumber(client, "UAa556f1a138444cf5359020bba46f017e", "+44778260206")
+        })
     
+    test("Attempt to free undeployed assistant", async () => {
+        let resp = await unlink.unlinkFromPhoneNumber(client, "")
+        expect(resp).toBe(false)
+        })
+    
+    test("Attempt to free null assistant", async () => {
+        let resp = await unlink.unlinkFromPhoneNumber(client, null)
+        expect(resp).toBe(false)
+        })
+    
+    test("Attempt to free undefined assistant", async () => {
+        let resp = await unlink.unlinkFromPhoneNumber(client, undefined)
+        expect(resp).toBe(false)
+        })
+})
+
+describe("Invalid Client Tests", () =>{
+    function invalidClient(client){
+        test("Invalid Client Definitions", () => {
+            unlink.unlinkFromPhoneNumber(client, "").catch(resp => {
+                expect(resp.name).toEqual("TwilioRequestError")
+            })
+        })
+    }
+
+    test("Invalid Twilio Credentials", () => {
+        let client = twilio("ACInvalid", "Credentials")
+        unlink.unlinkFromPhoneNumber(client, "").catch(resp => {
+            expect(resp.name).toEqual("TwilioRequestError")
+        })
+    })
+    
+    invalidClient(null)
+    invalidClient(undefined)
+})
+
     
